b4fa943f70327b9d32d64450a3741a619aadf0ba
Aaron Rondina <aaronrondina5@gmail.com>
2024-01-05 17:42:24 -0500

    add template fns to header

--- include/vector.h
+++ include/vector.h
@@ -253,7 +253,33 @@ private:
  * https://en.cppreference.com/w/cpp/algorithm/transform_reduce
 */
 template<typename T>
-T dot_product(const Vector<T>& lhs, const Vector<T>& rhs);
+T dot_product(const Vector<T>& rhs, const Vector<T>& lhs)
+{
+    if(lhs.dimensions() != rhs.dimensions())
+    {
+        throw std::runtime_error("cannot dot_product 2 different sizes.");
+    }
+    const int limit_size_for_parallelization = 1000;
+
+    if(lhs.dimensions() > limit_size_for_parallelization)
+    {
+        return std::transform_reduce(
+            std::execution::par
+            , lhs.begin()
+            , lhs.end()
+            , rhs.begin()
+            , T());
+    }
+    else
+    {
+        T result = T();
+        for(int i = 0; i < lhs.dimensions(); ++i)
+        {
+            result += (lhs[i] * rhs[i]);
+        }
+        return result;
+    }
+}
 
 /**
  * Determines if the vectors are perpendicular by computing 
@@ -261,7 +287,10 @@ T dot_product(const Vector<T>& lhs, const Vector<T>& rhs);
  * 
 */
 template<typename T>
-bool are_perpendicular(const Vector<T>& rhs, const Vector<T>& lhs);
+bool are_perpendicular(const Vector<T>& rhs, const Vector<T>& lhs)
+{
+    return dot_product(lhs, rhs) == 0;
+}
 
 } // vctr
 } // arondina
